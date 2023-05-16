5aecc32a0442bd5e2df586e0a5caacac3717ca45
joshkonecki@gmail.com <joshkonecki@gmail.com>
2023-05-16 12:14:07 -0400

    Add project files.

---
+++ QRGenerator/Program.cs
@@ -0,0 +1,6 @@
+﻿using IronBarCode;
+
+// Creating a barcode is as simple as:
+var myQRcode = QRCodeWriter.CreateQrCode("https://metalforall.azurewebsites.net", 500);
+// After creating a barcode, we may choose to resize and save which is easily done with:
+myQRcode.SaveAsImage("BeardedBelligerentsHomepage.jpg");
