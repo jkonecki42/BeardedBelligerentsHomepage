479a738287b03fe01659f26bd1b5df39f9b486e1
Josh Konecki <joshua.konecki@galenhealthcare.com>
2024-03-01 09:32:57 -0500

    added config value for testing tokenreplacer azure devops tool

--- BeardedBelligerentsHomepage/Pages/Index.cshtml.cs
+++ BeardedBelligerentsHomepage/Pages/Index.cshtml.cs
@@ -5,16 +5,21 @@ namespace BeardedBelligerentsHomepage.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Title;
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
+
+        
 
         public void OnGet()
         {
-
+            Title = _config["ChangeMe"];
         }
     }
 }
